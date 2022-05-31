13ba75862bed7a9fa75a3fdf4667c937568ca20b
sukeroV <he429855@gmail.com>
2022-05-31 13:06:05 +0800

    none

---
+++ 220531/T1More.lua
@@ -0,0 +1,25 @@
+print("数据类型")
+print(type("hello world"))
+print(type(10))
+print(type(print))
+print(type(true))
+print(type(nil))
+print(type(type(X)))
+print("////////////////////////////////////////////////////")
+print("循环")
+i = 10
+while (i >= 0) do
+    print(i)
+    i = i - 1
+end
+
+for i=10,9,-1 do
+    print(i)
+end
+
+a=10
+repeat
+    print(a)
+    a = a+1
+until (a>15)
+print("////////////////////////////////////////////////////")
