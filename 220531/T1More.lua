755e5661bae5f1f877790db2868372d14a2f21ac
sukeroV <he429855@gmail.com>
2022-05-31 13:38:14 +0800

    none

--- 220531/T1More.lua
+++ 220531/T1More.lua
@@ -23,3 +23,60 @@ repeat
     a = a+1
 until (a>15)
 print("////////////////////////////////////////////////////")
+print("流程控制")
+i = 3
+if (i == 0) then
+    print("i == 0")
+end
+
+if (i == 1) then
+    print("i == 1")
+else
+    print("i != 1,i =",i)
+end
+
+if (i == 2) then
+    print("i == 2")
+elseif (i == 3) then
+    print("i == 3")
+else
+    print("i ==",i)
+end
+print("////////////////////////////////////////////////////")
+print("函数")
+i = 0
+function max(num1,num2)
+    if (num1 > num2) then
+        print("num1 > num2")
+        return num1
+    elseif (num1<num2) then
+        print("num1<num2")
+        return num2
+    else
+        print("num1 == num2")
+        return num1
+    end
+end
+i = max(2100,1200)
+print(i)
+
+--可变参数
+function add(...)
+    for i,v in ipairs{...} do
+        print(v)
+    end
+end
+add(1,2,3,4,5,"he")
+print("////////////////////////////////////////////////////")
+print("运算符")
+print("or and not ~=")
+a,b = "hello","world"
+print("连接",a..b)
+print("b 字符串长度",#b)
+print("长度",#"heshaolang")
+print("整除 //",5//3)
+print("////////////////////////////////////////////////////")
+print("Lua String")
+
+
+print("////////////////////////////////////////////////////")
